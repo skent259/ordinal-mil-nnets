7bbcf724107168b802ad15ded44ef0362c213964
spkent <spkent@submit3.chtc.wisc.edu>
2022-11-20 11:58:51 -0600

    fix condor bugs, make condor_main.sh more clear

--- condor/condor_main.sh
+++ condor/condor_main.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 NAME=$1
-EXP_FILE=$2
+VERSION=$2
 I=$3
-OUTPUT_DIR="results/$NAME/"
+OUTPUT_DIR=$4
+EXP_FILE="experiment-${NAME}-${VERSION}.csv"
 
 ENVNAME=ordinal-mil-nnets
 ENVDIR=$ENVNAME
@@ -16,20 +17,19 @@ tar -xzf $ENVNAME.tar.gz -C $ENVDIR
 . $ENVDIR/bin/activate
 
 # Prepare folder structure
-tar -xzf to-transfer-$NAME.tar.gz
+tar -xzf to-transfer-${NAME}.tar.gz
 mkdir results
-mkdir results/$NAME
+mkdir results/${NAME}
 
 # run script
-# python3 fgnet-1.0.0.py --i $1 --output_dir $OUTPUT_DIR
 python3 condor_main.py -e $EXP_FILE --i $I --output_dir $OUTPUT_DIR
 
 # zip up output
-tar -czf $NAME-1.0.0_$1.tar.gz results/
-mv $NAME-1.0.0_$1.tar.gz /staging/spkent/ordinal-mil-nnets/
+tar -czf ${NAME}-${VERSION}_${I}.tar.gz results/
+mv ${NAME}-${VERSION}_${I}.tar.gz /staging/spkent/ordinal-mil-nnets/
 
 find $OUTPUT_DIR -name "*.hdf5" -exec rm {} \;
-tar -czf $NAME-1.0.0_sm_$1.tar.gz results/ 
+tar -czf ${NAME}-${VERSION}_sm_${I}.tar.gz results/ 
 
 # clean up
 rm $ENVNAME.tar.gz
--- condor/unzip.sh
+++ condor/unzip.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Unzips the directories returned from condor
-name="fgnet-1.0.0_sm_"
+name="fgnet-1.0.1_sm_"
 find . -name "$name*.tar.gz" -exec tar -xzf {} \;
