72a5425e35965726ccc752a103db29fde69a06c6
skent259 <skent259@gmail.com>
2022-11-24 07:39:52 -0600

    add aes-2.1.0 parameters

--- experiment/aes-params.py
+++ experiment/aes-params.py
@@ -78,6 +78,31 @@ experiment_202 = expand_grid(
 
 experiment_df_to_csv(experiment_202, "experiment/params/experiment-aes-2.0.2.csv")
 
+
+# aes-2.1.0
+experiment_210 = expand_grid(
+    {
+        "ordinal_method": [OrdinalType.CORAL, OrdinalType.CORN],
+        "mil_method": [MILType.CAP_MI_NET, MILType.MI_NET, MILType.CAP_MI_NET_DS],
+        "data_set_type": [DataSetType.AES],
+        "data_set_name": [
+            "aes_bag_wr=0.5_size=4_i=0_j=0",
+            "aes_bag_wr=0.5_size=4_i=0_j=1",
+            "aes_bag_wr=0.5_size=4_i=0_j=2",
+            "aes_bag_wr=0.5_size=4_i=0_j=3",
+            "aes_bag_wr=0.5_size=4_i=0_j=4",
+        ],
+        "batch_size": [1],
+        "learning_rate": [0.01, 0.001, 0.0001],
+        "epochs": [100],
+        "pooling_mode": ["max", "mean"],
+        "early_stopping": [False],
+    }
+)
+
+experiment_df_to_csv(experiment_210, "experiment/params/experiment-aes-2.1.0.csv")
+
+
 # # Test read in
 # test = pd.read_csv("experiment/params/experiment-aes-2.0.1.csv")
 # test["ordinal_method"] = [OrdinalType[x] for x in test["ordinal_method"]]
